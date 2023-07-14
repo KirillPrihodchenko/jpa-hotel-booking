06dbcfa379b02b6c3fbab756cc3bd126c30ae498
Kirill Prihodchenko <kirprihodchenko@gmail.com>
2023-07-14 18:27:22 +0300

    add repo and cteare database model diagram

---
+++ src/main/java/com/booking/jpahotelbooking/JpaHotelBookingApplication.java
@@ -0,0 +1,12 @@
+package com.booking.jpahotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHotelBookingApplication.class, args);
+    }
+}
